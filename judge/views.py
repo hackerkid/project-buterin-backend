1f1e778300b022b2e3d0e0537d2713a02b09d7ba
Vishnu Ks <yo@vishnuks.com>
2018-08-11 14:34:49 +0530

    get gas price api done.

--- judge/views.py
+++ judge/views.py
@@ -3,10 +3,13 @@ from django.http import HttpRequest, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.core.files import File
 
+from subprocess import call
+
 import os
 
 module_dir = os.path.dirname(__file__)
-uploads_dir = os.path.join(module_dir, "uploads")
+uploads_dir = os.path.join("uploads")
+testcase_dir = os.path.join("testcases")
 
 def handle_uploaded_file(f, file_name):
 
@@ -25,6 +28,17 @@ def handle_uploaded_file(f, file_name):
 def submit_code(request: HttpRequest):
     question_id = request.POST["question_id"]
     participant_id = request.POST["participant_id"]
-    file_name = os.path.join(uploads_dir, question_id, participant_id + ".sol")
-    handle_uploaded_file(request.FILES['code'], file_name)
-    return HttpResponse("done")
+
+    os.environ["question_id"] = question_id
+    os.environ["participant_id"] = participant_id
+
+    solution_file = os.path.join(uploads_dir, question_id, participant_id + ".sol")
+    gas_used_file = os.path.join(uploads_dir, question_id, participant_id + ".gas")
+    
+    testcase_file = os.path.join(testcase_dir, question_id + ".js")
+
+    handle_uploaded_file(request.FILES['code'], solution_file)
+    call(["node_modules/mocha/bin/mocha", testcase_file])
+
+    with open(gas_used_file, "r") as f:
+        return HttpResponse(f.read())
